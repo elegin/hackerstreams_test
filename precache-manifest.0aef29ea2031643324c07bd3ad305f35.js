9ba47972411abe23141d6ed97c10e3a280ece8f1
elegin <elegin@gmail.com>
2020-05-15 17:28:02 -0400

    Updates

---
+++ precache-manifest.0aef29ea2031643324c07bd3ad305f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5603326dc6624b19d1f7cb29cf2d04",
+    "url": "/hackerstreams_test/index.html"
+  },
+  {
+    "revision": "4013235f1bf78f1a0796",
+    "url": "/hackerstreams_test/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "5b5806451974d7b2f96e",
+    "url": "/hackerstreams_test/static/js/2.7f113e4e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/hackerstreams_test/static/js/2.7f113e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4013235f1bf78f1a0796",
+    "url": "/hackerstreams_test/static/js/main.5e58ca8a.chunk.js"
+  },
+  {
+    "revision": "1cb4ade6b0a312ff7a0a",
+    "url": "/hackerstreams_test/static/js/runtime-main.0470d420.js"
+  }
+]);
